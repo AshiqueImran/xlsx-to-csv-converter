1789b9fb7e785e6c7f758763c3d289376607040a
Md Ashique Imran <imranashik50@gmail.com>
2019-05-19 10:01:16 +0600

    dynamically sheet name added

--- script.py
+++ script.py
@@ -3,6 +3,16 @@ import csv
 
 def csv_from_excel():
     wb = xlrd.open_workbook('ON AIR SITES DETAILS _180828.xlsx')
+
+
+    # dynamically sheet name get
+
+    # sheet_names = xl_workbook.sheet_names()
+    # print('Sheet Names', sheet_names)   
+    # sh = xl_workbook.sheet_by_name(sheet_names[0])
+
+     # dynamically sheet name get ends
+
     sh = wb.sheet_by_name('DM sites')
     your_csv_file = open('your_csv_file.csv', 'wb')
     wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
